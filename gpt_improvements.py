07dbae757aafc0dd50de0c49aaef86a4b75cd382
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2023-12-23 16:00:23 +0100

    Switched to threads instead of AsyncIO

--- gpt_improvements.py
+++ gpt_improvements.py
@@ -1,9 +1,10 @@
+import argparse
+import logging
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
-import asyncio
 from openai import AsyncOpenAI
 from collections import defaultdict
-from suggestion_requests import SuggestionRequests
+from suggestion_request import SuggestionRequest
 
 load_dotenv()
 client = AsyncOpenAI()
@@ -12,41 +13,26 @@ app = Flask(__name__)
 requests = defaultdict(int)
 
 
-# Function to generate improvements based on user input using gpt-3.5-turbo
-async def generate_improvements(cover_letter, job_description):
-    print("booom")
-    completion_requests = SuggestionRequests(
-        cover_letter=cover_letter, job_description=job_description
-    )
-    requests[completion_requests.request_id] = {"data": vars(completion_requests)}
-
-    print(f"completion request: {vars(completion_requests)}")
-    await completion_requests.generate_improvements()
-    requests[completion_requests.request_id].update({"data": vars(completion_requests)})
-    return vars(completion_requests)
-
-
 @app.route("/requests", methods=["POST"])
 async def submit_suggestion():
     # Get your necessary data to make a new request
     cover_letter = request.json["cover-letter"]
     job_description = request.json["job-description"]
 
-    print(f"cover letter: {cover_letter}")
-    print(f"job: {job_description}")
-    response = asyncio.create_task(generate_improvements(cover_letter, job_description))
-    response = await response
-    print(response)
-    print("\n")
+    app.logger.debug(f"cover letter: {cover_letter}")
+    app.logger.debug(f"job: {job_description}")
+    my_request = SuggestionRequest(cover_letter, job_description)
+    requests[my_request.request_id] = my_request
+    my_request.start()
 
-    return response
+    return {"request_id": my_request.request_id}
 
 
 @app.route("/requests/<request_id>", methods=["GET"])
 def get_suggestion(request_id):
     # Your frontend knows the request id, it can use it to retrieve the status + data if there
-    print(requests)
-    return requests[request_id]["data"]
+    app.logger.debug(requests)
+    return requests[request_id].to_json()
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -56,7 +42,28 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-v",
+        "--verbose",
+        action="store_const",
+        dest="log_level",
+        const=logging.INFO,
+        default=logging.WARNING,
+    )
+    group.add_argument(
+        "-d",
+        "--debug",
+        action="store_const",
+        dest="log_level",
+        const=logging.DEBUG,
+        default=logging.WARNING,
+    )
+    args = parser.parse_args()
+    app.logger.setLevel(args.log_level)
+
+    app.run(debug=args.log_level == logging.DEBUG)
 
 
 ## API call tp openaiAPI does not work twice unless restarting
